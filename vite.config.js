95d46b99819ba8214fdb8ca14d7a484562c2c497
Haniche Zahir <zahir.haniche@inetum.com>
2024-08-22 02:26:33 +0200

    Change configuration vite

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/reactjs-job-list/', // Spécifiez le chemin de base pour GitHub Pages
-  build: {
-    outDir: 'dist',
-  },
+  base: '/reactjs-job-list/',
 });
